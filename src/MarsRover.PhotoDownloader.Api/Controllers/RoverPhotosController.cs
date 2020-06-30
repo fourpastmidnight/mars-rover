3bad6082a9affa4d495409d6c863313e07c994fa
fourpastmidnight <fourpastmidnight@users.noreply.github.com>
2020-06-30 09:28:04 -0400

    Adds initial WebAPI project for rover photos

---
+++ src/MarsRover.PhotoDownloader.Api/Controllers/RoverPhotosController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarsRover.PhotoDownload.Api.Controllers
+{
+    [Route("api/rover-photos")]
+    [ApiController]
+    public class RoverPhotosController : ControllerBase
+    {
+        public ActionResult GetRoverPhotos([FromQuery]uint skip = 0, [FromQuery]uint take = 10)
+        {
+            var skipping = skip * take;
+            var first = (skip * take) + 1;
+            var last = (skip * take) + take;
+            return Content($"Skipping {skipping} photos and getting photos {first} through {last}...");
+        }
+
+        [Route("{rover}/{date:datetime}")]
+        public ActionResult GetRoverPhotosByDate(Rover rover, DateTime date)
+        {
+            // TODO: Return JSONResult containing an array of available photo URLs for a given rover on a given date
+            // If there are no photos stored locally:
+            //   1. query the NASA API and obtain the list of URLS
+            //   2. If there are any photos available for the selected date and rover:
+            //      a. Transform the list of URLs to the list of local URLs where the photos can be obtained by this api
+            //         - return this list
+            //      b. Start a task that will begin retrieving the photos via the NASA API, storing them locally for retrieval
+            //   3. If no photos are available for the selected date and rover, return a 204 No Content.
+            return Content("No photos taken by this rover on that date.");
+        }
+
+        [Route("{date:datetime}")]
+        public ActionResult GetPhotosByDate(DateTime date)
+        {
+            // TODO: Return JSONResult containing an array of available photo URLs for a given date
+            // If there are no photos stored locally for any rover on the given date:
+            //   1. query the NASA API and obtain the list of URLS
+            //   2. If there are any photos available for the selected date and rover:
+            //      a. Transform the list of URLs to the list of local URLs where the photos can be obtained by this api
+            //         - return this list
+            //      b. Start a task that will begin retrieving the photos via the NASA API, storing them locally for retrieval
+            //   3. If no photos are available for the selected date and rover, return a 204 No Content.
+            return Content("No photos were taken on this date.");
+        }
+
+        [Route("{rover}")]
+        public ActionResult GetPhotosByRover(Rover rover)
+        {
+            // TODO: Return JSONResult containing an array of available photo URLs for a given date
+            // If there are no photos stored locally for a given rover, return 204 No Content
+            // Otherwise, return a list of available photo urls.
+            // NOTE: This call does not attempt to go out and retrieve any photos from NASA's API, as their api
+            // requires both a rover AND a date and there are thousands of photos available.
+            // Also, this application does not attempt to query the rover's mission manifest to know which dates are valid for a given rover.
+            return Content("Nothing to see here.");
+        }
+
+        [Route("download/{rover}/{date:datetime}/{filename}")]
+        public ActionResult DownloadRoverPhotos(Rover rover, DateTime date, string filename)
+        {
+            // Retrieves a cached photo by rover, date, and filename.
+            return Content("Not implemented yet.");
+        }
+
+        // TODO: Put all of this stuff in a testable, self-contained class...
+        private readonly HttpClient _httpClient;
+    }
+}
---
+++ src/MarsRover.PhotoDownloader.Api/Rover.cs
@@ -0,0 +1,9 @@
+﻿namespace MarsRover.PhotoDownload.Api
+{
+    public enum Rover
+    {
+        Curiosity = 5,
+        Opportunity = 6,
+        Spirit = 7
+    }
+}
